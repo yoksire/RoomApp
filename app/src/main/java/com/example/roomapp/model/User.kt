c5c79ab9c31cc9d6b78c16a7b9a6bb9abf831201
yoksire <79806563+yoksire@users.noreply.github.com>
2024-01-08 17:03:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/roomapp/model/User.kt
@@ -0,0 +1,27 @@
+package com.example.roomapp.model
+
+import android.os.Parcel
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.parcel.Parcelize
+
+@Parcelize
+@Entity(tableName = "user_table")
+data class User(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val firstName: String,
+    val lastName: String,
+    val age: Int
+): Parcelable {
+    override fun describeContents(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        TODO("Not yet implemented")
+    }
+}
+
+
